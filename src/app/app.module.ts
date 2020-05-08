bd5803b5a7073fdc8404a5401af2e4efd55ae07a
Abraham <abraham.castro@adp.com>
2020-05-08 15:04:03 +0200

    initial setup and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegistersComponent } from './components/registers/registers.component';
+import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegistersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
